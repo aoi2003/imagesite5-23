e03ad59809434982fd71bef92666cfd8a4445e86
A01 <koukou20030304@yahoo.co.jp>
2024-05-30 14:34:00 +0900

    config_urlの修正

--- config/urls.py
+++ config/urls.py
@@ -6,13 +6,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('image.urls')),  # 変更
+    path('', include('image.urls')), # 変更
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
